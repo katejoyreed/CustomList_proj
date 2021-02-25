eabe95d2e2c2c82bf5beed5549ccf87652bfe718
katejoyreed <katejoyreed@gmail.com>
2021-02-25 15:17:26 -0600

    Added unit test to verify ToString method converts custom list of bools to strings

--- CustomListToStringUnitTests/ToStringUnitTests.cs
+++ CustomListToStringUnitTests/ToStringUnitTests.cs
@@ -8,8 +8,30 @@ namespace CustomListToStringUnitTests
     public class ToStringUnitTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void To_String_Returns_Two_List_Int_In_One_String()
         {
+            CustomList<int> list = new CustomList<int>();
+            list.Add(1);
+            list.Add(2);
+            string expected = "1, 2";
+            string actual;
+
+            actual = list.ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void To_String_Return_Two_Bool_In_One_String()
+        {
+            CustomList<bool> list = new CustomList<bool>();
+            list.Add(true);
+            list.Add(false);
+            string expected = "true, false";
+            string actual;
+
+            actual = list.ToString();
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
