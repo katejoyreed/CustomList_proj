910972aa107bb9f4e91c7b6cfc3b8e218655df39
katejoyreed <katejoyreed@gmail.com>
2021-02-25 18:06:05 -0600

    Added + operator unit test to verify the count of the third list is equal to the count of both added lists

--- PlusOperatorUnitTests/PlusOperatorUnitTests.cs
+++ PlusOperatorUnitTests/PlusOperatorUnitTests.cs
@@ -23,5 +23,33 @@ namespace PlusOperatorUnitTests
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+
+        public void Plus_Operator_Third_List_Count_Correct()
+        {
+            CustomList<int> list1 = new CustomList<int>();
+            list1.Add(1);
+            list1.Add(1);
+            list1.Add(1);
+            list1.Add(1);
+            list1.Add(1);
+
+            CustomList<int> list2 = new CustomList<int>();
+            list2.Add(2);
+            list2.Add(2);
+            list2.Add(2);
+            list2.Add(2);
+            list2.Add(2);
+
+            CustomList<int> list3 = new CustomList<int>();
+            int expected = 10;
+            int actual;
+
+            list3 = list1 + list2;
+            actual = list3.Count;
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
