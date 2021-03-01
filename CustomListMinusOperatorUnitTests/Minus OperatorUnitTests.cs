d926c86b5d8e67000d8f81745f5718d59250284a
katejoyreed <katejoyreed@gmail.com>
2021-03-01 09:36:08 -0600

    Added - operator unit test to verify that multiple instances of shared integers are removed when list2 is subtracted from list1

--- CustomListMinusOperatorUnitTests/Minus OperatorUnitTests.cs
+++ CustomListMinusOperatorUnitTests/Minus OperatorUnitTests.cs
@@ -27,5 +27,27 @@ namespace CustomListMinusOperatorUnitTests
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Minus_Operator_Multiple_Instances_Of_Common_Int_Are_Removed()
+        {
+            CustList<int> list1 = new CustList<int>();
+            list1.Add(1);
+            list1.Add(3);
+            list1.Add(5);
+            list1.Add(7);
+            CustList<int> list2 = new CustList<int>();
+            list2.Add(1);
+            list2.Add(3);
+            list2.Add(5);
+            CustList<int> list3 = new CustList<int>();
+            int expected = 7;
+            int actual;
+
+            list3 = list1 - list2;
+            actual = list3[0];
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
