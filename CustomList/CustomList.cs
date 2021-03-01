38bcd5e96f5aa2a02037faa38988f8d33857fe1c
katejoyreed <katejoyreed@gmail.com>
2021-03-01 10:52:15 -0600

    Completed - operator overload method; passes all unit tests

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -113,16 +113,13 @@ namespace CustomList
 
         public static CustList<T> operator -(CustList<T> list1, CustList<T> list2)
         {
-            CustList<T> list3 = new CustList<T>();
+            
 
-            for (int i = 0; i < list1.count; i++)
+            for (int i = 0; i < list2.count; i++)
             {
-                if (!list1[i].Equals(list2[i]))
-                {
-                    list3.Add(list1[i]);
-                }
+                list1.Remove(list2[i]);
             }
-            return list3;
+            return list1;
         }
     }
 }
